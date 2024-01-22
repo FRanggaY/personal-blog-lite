9da136e60790bda517306d210ac4f8bf084d8032
FRanggaY <franciscusrangga@gmail.com>
2024-01-22 13:51:53 +0700

    update : add new article

--- src/utils/articleManagement.ts
+++ src/utils/articleManagement.ts
@@ -1,6 +1,7 @@
 import articleThreeData from '@/data/article/articleThreeData.json';
 import articleFourData from '@/data/article/articleFourData.json';
 import articleFiveData from '@/data/article/articleFiveData.json';
+import articleSixData from '@/data/article/articleSixData.json';
 
 export const selectArticleData = (slug:any, lang:any) => {
   switch (true) {
@@ -10,6 +11,8 @@ export const selectArticleData = (slug:any, lang:any) => {
       return articleFourData;
     case slug === 'ubuntu-20-04-manage-postgresql-12' && lang === 'en':
       return articleFiveData;
+    case slug === 'ubuntu-20-04-manage-nginx-1-18' && lang === 'en':
+      return articleSixData;
     default:
       return '';
   }
